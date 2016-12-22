0c4972815d680dcf01dbde9aec128516f554f39d
Wendell Valdez <vinduhl@gmail.com>
2016-12-22 16:20:09 -0600

    updates

---
+++ src/components/Login/Login.js
@@ -0,0 +1,63 @@
+import React from "react";
+import { connect } from "react-redux";
+import { browserHistory } from "react-router";
+import { login } from "../../ducks/userDuck";
+
+import "./Login.css";
+
+class Login extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: ""
+      , email: ""
+    }
+  }
+
+  handleChange(field, event) {
+    this.setState( { [field]: event.target.value } );
+  }
+
+  login(event) {
+    event.preventDefault();
+
+    this.props.dispatch( login( {
+      username: this.state.username
+      , email: this.state.email
+    }));
+  }
+
+  render() {
+    return(
+      <div>
+        <form className="login-form">
+            <input
+                onChange={ this.handleChange.bind( this, "username" ) }
+                placeholder="Username"
+                type="text"
+                value={ this.state.username }
+            />
+            <input
+                onChange={ this.handleChange.bind( this, "email" ) }
+                placeholder="Email"
+                type="text"
+                value={ this.state.email }
+            />
+            <button
+                onClick={ this.login.bind( this )}
+                type="submit"
+            >
+                Login
+            </button>
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default connect( state => ({
+  user: state.user
+}) )(Login);
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import { Router, Route, browserHistory } from "react-router";
 
 import store from "./store";
 import { Provider } from "react-redux";
-import App from "./components/app";
+import App from "./components/App/App";
+import Login from "./components/Login/Login"
 
 document.addEventListener( "DOMContentLoaded", () => {
   const reactNode = document.getElementById("react-node");
@@ -15,6 +16,7 @@ document.addEventListener( "DOMContentLoaded", () => {
       <Provider store={ store }>
         <Router history={ browserHistory }>
           <Route path="/" component={ App }>
+            <Route path="login" component={ Login }>
           </Route>
         </Router>
       </Provider>
